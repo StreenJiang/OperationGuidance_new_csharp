033107312c69692ad4329b5460fe44ca7da46f49
StreenJiang <streen.jiang@outlook.com>
2025-03-15 15:45:42 +0800

    Change file type for challenge result from *.res to *.result

--- OperationGuidance_new/Utils/ChallengeTaskUtil.cs
+++ OperationGuidance_new/Utils/ChallengeTaskUtil.cs
@@ -1,5 +1,5 @@
 namespace OperationGuidance_new.Utils {
     public class ChallengeTaskUtil: ASettingsFileUtil {
-        public ChallengeTaskUtil() : base(MainUtils.GetBaseDirectory(), "ChallengeResult", ".res") { }
+        public ChallengeTaskUtil() : base(MainUtils.GetBaseDirectory(), "ChallengeResult", ".result") { }
     }
 }
